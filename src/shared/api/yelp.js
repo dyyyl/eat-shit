dcf5cf411ab047f2d9c59881db138dfdc08e7ce1
dyyyl <hey@dyyyl.rocks>
2019-06-23 22:37:03 +0200

    yelp api

---
+++ src/shared/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const yelp = axios.create({
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3',
+  headers: {
+    Authorization: process.env.REACT_APP_YELP,
+  },
+});
+
+export default yelp;
