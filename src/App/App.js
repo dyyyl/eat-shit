722f0fbff137bcb457ff29a38b449abc4e691166
dyyyl <hey@dyyyl.rocks>
2019-07-01 17:55:33 +0200

    abstract logic

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
-import yelp from 'shared/api/yelp';
+import Header from 'shared/components/Header';
+import fetchData from 'shared/helpers/fetchData';
 import GlobalStyle from 'shared/styles/GlobalStyle';
 
 const App = () => {
-  const fetchData = async () => {
-    const response = await yelp.get('/businesses/search', {
-      params: {
-        location: 'berlin',
-        term: 'tacos',
-        limit: 5,
-      },
-    });
-    console.log(response);
-  };
-
   fetchData();
   return (
     <>
-      <h1>
-        Eat Shit{' '}
-        <span role="img" aria-label="kiss">
-          😘
-        </span>
-      </h1>
+      <Header />
       <GlobalStyle />
     </>
   );
