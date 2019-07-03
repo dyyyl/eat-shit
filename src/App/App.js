2df4c8ce7ad7bf565696a68d7dc91131454f700a
LashaunnaS <shaunna191@icloud.com>
2019-07-03 19:18:58 +0200

    Added Layout component.

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from 'shared/components/Header';
 import fetchData from 'shared/helpers/fetchData';
 import GlobalStyle from 'shared/styles/GlobalStyle';
+import Layout from './Layout';
 
 const App = () => {
   fetchData();
   return (
     <>
       <Header />
+      <Layout />
       <GlobalStyle />
     </>
   );
