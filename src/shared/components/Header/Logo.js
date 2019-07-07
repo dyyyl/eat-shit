bc7b51e8220fcd427d4f54a1c5b137abb2c19851
dyyyl <hey@dyyyl.rocks>
2019-07-07 16:32:57 +0200

    vague header responsivity

--- src/shared/components/Header/Logo.js
+++ src/shared/components/Header/Logo.js
@@ -6,6 +6,20 @@ const Logo = styled.h1`
   color: white;
   text-transform: uppercase;
   text-decoration: none;
+
+  @media (max-width: 440px) {
+    font-size: 2.5rem;
+  }
+
+  @media (max-width: 330px) {
+    font-size: 1.5rem;
+    padding: 0.25rem 0.5rem;
+  }
+
+  @media (max-width: 258px) {
+    font-size: 1.25rem;
+    padding: 0.25rem 0.5rem;
+  }
 `;
 
 export default Logo;
